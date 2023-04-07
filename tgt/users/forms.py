811501e9c4bbaff220e0fa70d47ff5fc7d487b80
Satrey <satrey.mail@gmail.com>
2023-04-07 11:44:02 +0500

    Добавлены формы авторизации.

--- tgt/users/forms.py
+++ tgt/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 
 from .models import CustomUser
 
@@ -18,3 +18,22 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         # fields = ('username', 'password', 'email', 'first_name', 'middle_name', 'last_name', 'department')
+
+
+class UserAuthForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(AuthenticationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+
+
+
+# class UserCreateForm(UserCreationForm):
+#     def __init__(self, *args, **kwargs):
+#         super(UserCreationForm, self).__init__(*args, **kwargs)
+
+#         for fieldname in ['username', 'password1', 'password2']:
+#             self.fields[fieldname].help_text = None
+#             self.fields[fieldname].widget.attrs.update({'class': 'form-control'})
+
+
