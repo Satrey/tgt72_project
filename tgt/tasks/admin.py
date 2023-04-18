595714a8add10f4df78bc9effe8f1f0ac27f830b
Satrey <satrey.mail@gmail.com>
2023-04-18 17:57:13 +0500

    Добавлена модель Task в приложение задачи.

--- tgt/tasks/admin.py
+++ tgt/tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'task_start_date', 'task_crossroad_address', 'task_bug_description', 'task_sender',
+                    'task_closed']
+    list_filter = ['task_crossroad_address', 'task_bug_description', 'task_sender', 'task_start_date', 'task_closed']
+
+
+admin.site.register(Task, TaskAdmin)
--- tgt/tasks/models.py
+++ tgt/tasks/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 
 
-# class Task(models.Model):
-#     start_date = models.DateTimeField(verbose_name='Время поступления')
-#     end_date = models.DateTimeField(verbose_name='Время завершения')
+class Task(models.Model):
+    """ Модель представляющая текущие задачи организации """
+
+    task_start_date = models.DateTimeField(auto_now_add=True,
+                                           verbose_name='Время поступления')
+    task_crossroad_address = models.ForeignKey('cross.CrossRoad', blank=True, null=True, on_delete=models.CASCADE,
+                                               verbose_name='Перекрёсток')
+    task_bug_description = models.TextField(blank=True, null=True, verbose_name='Описание ошибки')
+    task_sender = models.CharField(max_length=50, blank=True, null=True, verbose_name='Передал заявку')
+    task_receiver = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, blank=True, null=True,
+                                      verbose_name='Принял заявку')
+    task_act_number = models.IntegerField(null=True, blank=True, verbose_name='Номер акта наряда')
+    task_work_description = models.TextField(null=True, blank=True, verbose_name='Выполненные работы')
+    task_end_date = models.DateTimeField(auto_now=True, blank=True, null=True,
+                                         verbose_name='Время завершения')
+    task_closed = models.BooleanField(default=False, verbose_name='Закрыта')
+
+    def __str__(self):
+        return self.task_bug_description
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
